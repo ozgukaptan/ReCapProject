a8e3fbd85e3e07163334372fb13750576ca03d6a
ozgu kaptan <ozgukp@hotmail.com>
2021-02-19 17:12:05 +0300

    aa

---
+++ Entities/Dtos/RentalDetail.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Dtos
+{
+    public class RentalDetail : IDto
+    {
+
+    }
+}
