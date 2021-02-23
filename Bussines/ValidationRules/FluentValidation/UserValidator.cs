784b115f60e48994f31e4b1457727b73c9d271d5
ozgu kaptan <ozgukp@hotmail.com>
2021-02-24 02:50:59 +0300

    24-02-2021

---
+++ Bussines/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Bussines.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).MinimumLength(3);
+
+            RuleFor(u => u.FirstName).Must(StartWithA).WithMessage("Kullanıcı adı A harfi ile başlamalı");
+        }
+
+        private bool StartWithA(string arg)
+        {
+           return arg.StartsWith("A");
+        }
+    }
+}
