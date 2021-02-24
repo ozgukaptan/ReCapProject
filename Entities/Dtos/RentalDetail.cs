f76e09be44e8edb134427423528158736dd79bc1
ozgu kaptan <ozgukp@hotmail.com>
2021-02-24 03:57:23 +0300

    24022021

--- Entities/Dtos/RentalDetail.cs
+++ Entities/Dtos/RentalDetail.cs
@@ -7,6 +7,15 @@ namespace Entities.Dtos
 {
     public class RentalDetail : IDto
     {
-
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public int CustomerId { get; set; }
+        public string Description { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public string UserName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
