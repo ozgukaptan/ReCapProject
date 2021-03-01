c8fb6111aff5f3471a1debc0f97f020487a43030
ozgu kaptan <ozgukp@hotmail.com>
2021-03-01 17:38:06 +0300

    01032021

---
+++ Bussines/Abstract/ICarImageService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussines.Abstract
+{
+    public interface ICarImageService
+    {
+        IDataResult<CarImage> Add();
+        IDataResult<List<CarImage>> GetAll();
+        IDataResult<CarImage> GetCarImageById(int id);
+        IDataResult<CarImage> Update();
+        IResult Delete();
+    }
+}
